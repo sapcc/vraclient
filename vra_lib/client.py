e1250c61fc518476bad8fe1226c50d6731a286d4
Teodor Simchev <tsimchev@vmware.com>
2021-04-05 10:28:57 +0300

    Introduce base client

---
+++ vra_lib/client.py
@@ -0,0 +1,136 @@
+import eventlet
+import json
+import requests
+import logging
+from requests.exceptions import HTTPError
+from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectTimeout
+from vra_lib.synchronization import Scheduler
+
+LOG = logging.getLogger(__name__)
+
+class RetryPolicy(object):
+
+    def __call__(self, func):
+
+        def decorator(self, *args, **kwargs):
+            function = "{}.{}".format(self.__class__.__name__, func.__name__)
+            pattern_retry = "Retrying connection ({}/{}) with timeout {}s for {}"
+            pattern_error = "HTTP Response URL={} Code={} Reason={} Content={}"
+
+            until = self._config.connection_retries
+            pause = self._config.connection_retries_seconds
+            logger = self.logger
+
+            info = "Login"
+            if 'path' not in kwargs or 'login' not in kwargs.get('path'):
+                info = "Function {} Arguments {}".format(function, str(kwargs))
+
+            last_err = None            
+            for attempt in range(1, until + 1):
+                try:
+                    response = func(self, *args, **kwargs)
+                    if 200 <= response.status_code < 300 or response.status_code == 404:
+                        return response
+                    last_err = pattern_error.format(response.url, response.status_code,
+                                                    response.reason, response.content)
+                    if response.status_code >= 400 and response.status_code < 500:
+                        if "Login" in info:
+                            raise HttpUnsuccessfulException(last_err)
+                        self.login()
+                        continue
+                    if response.status_code >= 300:
+                        raise HttpUnsuccessfulException(last_err)
+                    return response
+                except (HTTPError, ConnectionError, ConnectTimeout) as err:
+                    last_err = err
+                    logger.error("Request={} Response={}".format(info, last_err))
+
+                logger.info(pattern_retry.format(attempt, until, pause, function))
+                eventlet.sleep(pause)
+            raise Exception(info, last_err)
+
+        return decorator
+
+
+class VraClientConfig:
+    host = None
+    port = None
+    username = None
+    password = None
+    organization = None
+    connection_retries = None
+    connection_retries_seconds = None
+    connection_timeout_seconds = None
+    connection_throttling_rate = None
+    connection_throttling_limit_seconds = None
+    connection_throttling_timeout_seconds = None
+    connection_query_limit = None
+    connection_certificate_check = None
+    spoof_guard = None
+    cloud_zone = None
+    logger = None
+
+
+class VraClient:
+    """
+    Client for vRealize Automation 8.x
+    """
+    LOGIN_API = "/csp/gateway/am/api/login"
+    BLUEPRINT_REQUESTS_API = "/blueprint/api/blueprint-requests"
+    BLUEPRINTS_API = "/blueprint/api/blueprints"
+
+    def __init__(self, vraClientConfig):
+        self._config = c = vraClientConfig
+
+        self.logger = c.logger if c.logger else LOG
+        self.api_scheduler = Scheduler(
+            c.connection_throttling_rate,
+            c.connection_throttling_limit_seconds,
+            c.connection_throttling_timeout_seconds)
+       
+        self.base_url = "https://{}:{}".format(c.host, c.port)
+        self.login = {
+            "username": c.username, 
+            "password": c.password, 
+            "domain": c.domain
+        }
+        
+        self.session = requests.session()
+        self.session.verify = c.connection_certificate_check
+        if not c.connection_certificate_check:
+            requests.packages.urllib3.disable_warnings()
+        self.timeout = c.connection_timeout_seconds
+        
+
+    def _get_url(self, path):
+        return "{}{}".format(self.base_url, path)
+
+    @RetryPolicy()
+    def get(self, path):
+        with self.api_scheduler:
+            return self.session.get(url=self._get_url(path))
+
+    @RetryPolicy()
+    def delete(self, path):
+        with self.api_scheduler:
+            return self.session.delete(url=self._get_url(path))
+
+    @RetryPolicy()
+    def post(self, path, json):
+        with self.api_scheduler:
+            return self.session.post(url=self._get_url(path), json=json)
+
+    @RetryPolicy()
+    def put(self, path, json):
+        with self.api_scheduler:
+            return self.session.put(url=self._get_url(path), json=json)
+
+    def login(self):
+        self.logger.info("Acquiring vRA token from {} ...".format(self.base_url))
+        r = json.loads(self.post(path=self.LOGIN_API, json=self.login))
+        self.session.headers.update({
+            'Authorization': 'Bearer {}'.format(r.content.get("cspAuthToken"))
+        })
+
+        self.logger.info("vRA token acquired")
---
+++ vra_lib/synchronization.py
@@ -0,0 +1,82 @@
+"""
+Synchronization - classes related concurrent execution scheduling and limits
+"""
+import os
+import time
+import functools
+import collections
+import logging
+from enum import Enum
+if not os.environ.get('DISABLE_EVENTLET_PATCHING'):
+    import eventlet
+    eventlet.monkey_patch()
+
+LOG = logging.getLogger(__name__)
+
+
+class Scheduler(object):
+    """ Synchronization.Scheduler.class limits the rate of execution of
+        'with' section
+
+        Keyword arguments:
+        rate -- the rate of execution
+        limit -- the limit of execution
+    """
+
+    def __init__(self, rate=1, limit=1.0, timeout=1, logger=None):
+
+        if limit <= 0:
+            raise ValueError('Schedule limit "{}" not positive'.format(limit))
+        if rate <= 0:
+            raise ValueError('Schedule rate "{}" not positive'.format(rate))
+
+        self.schedule = collections.deque()
+
+        self.rate = rate
+        self.limit = limit
+        self.timeout = timeout
+        self.log = logger if logger else LOG
+
+        # Callback reporting the limit was hit
+        def callback(seconds):
+            LOG.warning('Vra API Limit {:d}/s was hit. Sleeping for {:f}s.'
+                        .format(limit, seconds))
+
+        self.callback = callback
+        self._semaphore = eventlet.semaphore.Semaphore(value=self.rate)
+    
+    def log(self, message):
+        if self._logger:
+            return self.logger
+        else:
+
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+        return wrapped
+
+    def __enter__(self):
+        if self._semaphore.acquire(blocking=True, timeout=self.timeout):
+            run_time = time.time()
+            offset = len(self.schedule) - self.rate
+
+            if offset >= 0 and run_time - self.limit < self.schedule[offset]:
+                sleeptime = run_time - self.schedule[offset] + self.limit
+                if self.callback:
+                    eventlet.spawn(self.callback, sleeptime)
+                eventlet.greenthread.sleep(sleeptime)
+                run_time = self.schedule[offset] + self.limit
+            self.schedule.append(run_time)
+            return self
+        raise Exception("{} Queue Size={}, Rate={}, Limit={}, Timeout={}"\
+            .format("Timeout reached of trying to schedule operation.",
+                    len(self.schedule), self.rate, self.limit, self.timeout))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._semaphore.release()
+        now = time.time()
+        while self.schedule and self.schedule[0] < now - self.limit:
+            self.schedule.popleft()
